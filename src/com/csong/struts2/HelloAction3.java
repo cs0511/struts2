543e43f7c16c037c542f9562860c1290f018305d
cs0511 <cs0511@126.com>
2013-11-24 16:13:44 +0800

    add parameter for add

--- src/com/csong/struts2/HelloAction3.java
+++ src/com/csong/struts2/HelloAction3.java
@@ -3,13 +3,33 @@ package com.csong.struts2;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class HelloAction3 extends ActionSupport {
+	private String name;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	private Integer age;
+
 	public String execute() {
 		System.out.println("hello action3");
 		return "success";
 	}
 
 	public String add() {
-		System.out.println("hello action3 add");
+		System.out.println("age: " +  age);
+		System.out.println("name: " +  name);
 		return "success";
 	}
 }
